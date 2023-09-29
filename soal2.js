84201c636975a59330f9e98741fdc8ecf0084278
Poodoop <dickyaristyo43@gmail.com>
2023-09-29 18:47:44 +0700

    soal2.js program finished

--- soal2.js
+++ soal2.js
@@ -1,1 +1,20 @@
-var fs = require('fs')
+const fs = require('fs');
+
+function logToFile(logMessage) {
+  const timestamp = new Date().toLocaleString();
+  const logEntry = `${timestamp} ${logMessage}\n`;
+
+  fs.appendFile('log.txt', logEntry, (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+    } else {
+      console.log('Entry added:', logEntry);
+    }
+  });
+}
+
+logToFile('Sample log message 1');
+logToFile('Sample log message 2');
+logToFile('Sample log message 3');
+logToFile('Sample log message 4');
+logToFile('Sample log message 5');
